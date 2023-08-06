4300e604363fdc0bd2c094fb4f6b265bfa013f42
Aref <Arefkeyhani.pr@gmail.com>
2023-08-06 03:46:20 +0330

    mute button works now

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ class Drumkit {
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
-    this.selects = document.querySelectorAll("select");
+
     //this needs to be changed later
     this.bpm = 150;
     //
     this.isPlaying = null;
+    this.selects = document.querySelectorAll("select");
+    this.muteBtns = document.querySelectorAll(".mute");
   }
   activePad() {
     this.classList.toggle("active");
@@ -79,6 +81,35 @@ class Drumkit {
         break;
     }
   }
+  mute(event) {
+    const muteIndex = event.target.getAttribute("data-track");
+    event.target.classList.toggle("active");
+    if (event.target.classList.contains("active")) {
+      switch (muteIndex) {
+        case "0":
+          this.kickAudio.volume = 0;
+          break;
+        case "1":
+          this.snareAudio.volume = 0;
+          break;
+        case "2":
+          this.hihatAudio.volume = 0;
+          break;
+      }
+    } else {
+      switch (muteIndex) {
+        case "0":
+          this.kickAudio.volume = 1;
+          break;
+        case "1":
+          this.snareAudio.volume = 1;
+          break;
+        case "2":
+          this.hihatAudio.volume = 1;
+          break;
+      }
+    }
+  }
 }
 
 //Event Listerens
@@ -99,5 +130,11 @@ drumKit.playBtn.addEventListener("click", () => {
 drumKit.selects.forEach((select) => {
   select.addEventListener("change", function (event) {
     drumKit.changeSound(event);
+  });
+});
+
+drumKit.muteBtns.forEach((btn) => {
+  btn.addEventListener("click", function (event) {
+    drumKit.mute(event);
   });
 });
